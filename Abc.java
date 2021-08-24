a1dfc13b66ed73206d0002b9d5b75d56e379ac02
mohan <mohanreddy7337@gmail.com>
2021-08-24 11:13:45 +0530

    Mu second day code

--- Abc.java
+++ Abc.java
@@ -2,6 +2,7 @@ class Abc
 {
 public static void main(String[] args)
 {
+int[] b={4,8,9,4,7};
 int[] a=new int[4];
 a[0]=1;
 a[1]=2;
@@ -9,5 +10,11 @@ a[2]=3;
 a[3]=4;
 for(int i=0;i<4;i++)
 System.out.println(a[i]);
+m1(b);
+}
+static void m1(int[] a)
+{
+for(int n:a)
+System.out.println(n);
 }
 }
